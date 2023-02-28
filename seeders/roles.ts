6844b26f7b9eec79bf148df46898a14ea7370ead
QuangQuyen <quangquyendad@gmail.com>
2023-02-28 21:46:57 +0700

    q

---
+++ seeders/roles.ts
@@ -0,0 +1,14 @@
+export const roles = [
+    {
+        UserId:1,
+        PermissionId:2
+    },
+    {
+        UserId: 2,
+        PermissionId:1 
+    },
+    {
+        UserId:3,
+        PermissionId:2
+    }
+]
